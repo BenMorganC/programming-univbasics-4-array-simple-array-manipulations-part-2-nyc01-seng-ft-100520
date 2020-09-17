1fb5ecbb4703aa8583eebdc28bd443726dc4a6aa
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-17 14:35:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,19 @@ end
 def using_insert(array1, thing)
   array1.insert(4, thing)
 end
+
+def using_uniq(array)
+    array.uniq
+end 
+
+def using_flatten(array)
+    array.flatten 
+end 
+
+def using_delete(array, string)
+    array.delete(string)
+end 
+
+def using_delete_at(array, integer)
+    array.delete(integer)
+end 
